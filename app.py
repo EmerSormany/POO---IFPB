bd2cdec9a03c78a81b220cc187ddbd36a4763aa2
Emerson Sormany <sormanyemerson@gmail.com>
2025-04-22 18:23:07 +0000

    feat: exercício da aula inaugural

--- app.py
+++ app.py
@@ -8,12 +8,28 @@ class Balde:
     Terceiro argumento é o material de construção do balde em string.
     Quarto argumento é a cor do balde em string.
     """
+    
     def __init__(self, marca, volume, feito_de, cor):
         self.marca = marca
         self.volume = volume
         self.feito_de = feito_de
-        self.cor = cor
+        self.__cor = cor
+
+    @property
+    def cor(self):
+        return self.__cor
+
+    @cor.setter
+    def cor(self, cor):
+        self.__cor = cor
+
+
+    
 
 balde = Balde("Stanley", 8000, "Plástico", "Branca")
 
-print(f"Balde da marca {balde.marca}, com volume {balde.volume/1000} litros, construído em {balde.feito_de} e na cor {balde.cor}")
+balde.cor = 'preto'
+
+print(balde.cor)
+
+#print(f"Balde da marca {balde.marca}, com volume {balde.volume/1000} litros, construído em {balde.feito_de} e na cor {balde.cor}")
