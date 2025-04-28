1b87a8b7605172e235d4e7859fb1fe1941fa8796
Emerson Sormany <sormanyemerson@gmail.com>
2025-04-28 23:14:22 +0000

    feat: resolução do segundo exercício

---
+++ cap_1/e_2.py
@@ -0,0 +1,91 @@
+"""
+    1.2 Criando atributos e métodos usando atributos
+"""
+class Personagem:
+    """
+        6 Crie uma classe Personagem com um atributo nome e um
+        método dizer_nome() que imprime "Meu nome é {nome}".
+        Instancie um personagem e teste o método.
+
+        8 Crie uma classe Personagem com um atributo vida, inicializado
+        em 100. Adicione um método tomar_dano(dano), que reduz a
+        vida do personagem pelo valor passado como parâmetro. Se a
+        vida chegar a 0, imprima "Game Over!".
+    """
+
+    def __init__(self,nome , vida=100):
+        self.nome = nome
+        self.vida = vida
+
+    def tomar_dano(self, dano):
+        self.vida -= dano
+        if self.vida <= 0:
+            print("Game Over!")
+
+    def dizer_nome(self):
+        print(f"Meu nome é {self.nome}")
+
+persnagem = Personagem("Sibito")
+persnagem.dizer_nome()
+
+class Pontuacao:
+    """
+        7 Crie uma classe Pontuacao com um atributo pontos, inicializado
+        em 0, e dois métodos: adicionar_pontos(quantidade), que soma
+        quantidade ao total de pontos, e mostrar_pontos(), que imprime
+        a pontuação atual.
+    """
+    pontos = 0
+
+    def adicionar_pontos(self, quantidade):
+        self.pontos += quantidade
+    
+    def mostrar_pontos(self):
+        print(f"Pontuação atual: {self.pontos}")
+
+pontos = Pontuacao()
+pontos.adicionar_pontos(10)
+pontos.mostrar_pontos()
+
+class Jogador():
+    """
+        9 Crie uma classe Jogador com um atributo energia, inicializado
+        em 50. Adicione métodos recuperar_energia(quantidade) e
+        usar_energia(quantidade). Se a energia for menor que 0,
+        imprima "Sem energia suficiente!".
+    """
+    def __init__(self, energia=50):
+        self.energia = energia
+
+    def recuperar_energia(self, quantidade):
+        self.energia += quantidade
+
+    def usar_energia(self, quantidade):
+        if self.energia - quantidade < 0:
+            print("Sem energia suficiente!")
+        else:
+            self.energia -= quantidade
+
+jogador = Jogador()
+jogador.recuperar_energia(20)
+jogador.usar_energia(30)
+jogador.usar_energia(50)
+
+class Inimigo(Personagem):
+    """
+        10 Crie uma classe Inimigo com atributos nome e vida (inicializada
+        com 100). Adicione um método atacar(alvo), onde alvo é um
+        objeto da classe Personagem. Esse método deve reduzir a vida
+        do alvo em 10 pontos.
+    """
+    def __init__(self, nome, vida=100):
+        self.nome = nome
+        self.vida = vida
+
+    def atacar(self, alvo):
+        alvo.tomar_dano(100)
+
+heroi = Personagem("Sibito")
+inimigo = Inimigo("Ogro")
+
+inimigo.atacar(heroi)
